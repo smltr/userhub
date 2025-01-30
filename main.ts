15f3721c742e5377a50b5df00cb6ef7eb2255e58
Sam Trouy <smltr@proton.me>
2025-01-30 17:21:36 -0500

    fix urls

--- main.ts
+++ main.ts
@@ -1,16 +1,11 @@
 import { serve } from "https://deno.land/std@0.204.0/http/server.ts";
 
 async function handler(req: Request): Promise<Response> {
-  const url = new URL(req.url);
-
-  // Serve index.html
   try {
     const html = await Deno.readFile(new URL("./index.html", import.meta.url));
     return new Response(html, {
       headers: {
         "content-type": "text/html",
-        // Add CORS headers if needed
-        "access-control-allow-origin": "*",
       },
     });
   } catch (e) {
